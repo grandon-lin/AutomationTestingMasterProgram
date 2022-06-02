84a069e7b3954b531183947aa3ed05fc10f40b5c
Grandon Lin <grandonlin@gmail.com>
2022-06-02 16:49:39 -0400

    Added ForLoop and EnhancedForLoop

---
+++ mainPack/EnhancedForLoop.java
@@ -0,0 +1,16 @@
+package mainPack;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {12, 23, 44, 56, 78};
+		
+		for(int i : arr)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
