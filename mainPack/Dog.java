9adda6593aa35d5ff7225b41789037e4966fbb3a
Grandon Lin <grandonlin@gmail.com>
2022-06-02 16:56:02 -0400

    Added Dog and Puppy

---
+++ mainPack/Dog.java
@@ -0,0 +1,22 @@
+package mainPack;
+
+public class Dog {
+	String breed;
+	int age;
+	String color;
+	
+	void barking()
+	{
+		
+	}
+	
+	void hungry()
+	{
+		
+	}
+	
+	void sleeping()
+	{
+		
+	}
+}
