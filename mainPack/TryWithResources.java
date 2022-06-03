aba92ad99c687a716c9558f8aa1979ae7877fd60
Grandon Lin <grandonlin@gmail.com>
2022-06-02 21:53:32 -0400

    Added TrywithResources

---
+++ mainPack/TryWithResources.java
@@ -0,0 +1,22 @@
+package mainPack;
+
+import java.io.FileOutputStream;
+
+public class TryWithResources {
+
+	public static void main(String[] args) {
+		
+		try(FileOutputStream fileOutputStream = new FileOutputStream("abc.txt"))
+		{
+			String msg = "Welcome to java!";
+			byte byteArray[] = msg.getBytes(); //converting string into byte array
+			fileOutputStream.write(byteArray);
+			System.out.println("Message written to file successfully!");
+		} catch (Exception exception)
+		{
+			System.out.println(exception);
+		}
+
+	}
+
+}
